91b43bec1746bdf8edd4fa857b0e635e57da9bd6
Mehul kajavadara <mehulkajavadara100@gmail.com>
2026-08-06 06:32:11 +0530

    First API

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "message": "Welcome to My API"
+    }
+
+@app.get("/hello")
+def hello():
+    return {
+        "message": "Hello Mehul"
+    }
